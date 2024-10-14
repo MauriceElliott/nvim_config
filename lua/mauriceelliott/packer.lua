5cfe337a571772e0f49c79743c0166d93b9df1b6
MauriceElliott <maurice.elliott@telusagcg.com>
2024-10-14 12:04:09 +0100

    Changed theme to tokyonight

--- lua/mauriceelliott/packer.lua
+++ lua/mauriceelliott/packer.lua
@@ -1,4 +1,4 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
+--This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
@@ -12,10 +12,10 @@ return require('packer').startup(function(use)
   	  	requires = { {'nvim-lua/plenary.nvim'} }
 	  }
 	  use({
-		'rose-pine/neovim',
-		  as = 'rose-pine',
+		'folke/tokyonight.nvim',
+		  as = 'tokyonight',
 		  config = function()
-		 	vim.cmd('colorscheme rose-pine')
+		 	vim.cmd('colorscheme tokyonight')
 		  end
 	  })
 end)
