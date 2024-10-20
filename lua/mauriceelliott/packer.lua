62455184be1019dda90199ced6fc513cf2908af3
MauriceElliott <mauricemelliott@gmail.com>
2024-10-21 00:08:42 +0100

    Added lsp for lua

--- lua/mauriceelliott/packer.lua
+++ lua/mauriceelliott/packer.lua
@@ -48,4 +48,6 @@ return require('packer').startup(function(use)
     'nvim-lualine/lualine.nvim',
     requires = { 'nvim-tree/nvim-web-devicons', opt = true }
     }
+    use 'neovim/nvim-lspconfig'
+    use 'nvim-treesitter/nvim-treesitter'
 end)
